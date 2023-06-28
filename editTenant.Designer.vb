46a5fe4a35c2d4b748d91aed1a27665ae0d81d13
obuyajr <felix37brock@gmail.com>
2023-06-28 15:29:07 +0300

    editHouse

--- editTenant.Designer.vb
+++ editTenant.Designer.vb
@@ -25,24 +25,24 @@ Partial Class editTenant
         Panel1 = New Panel()
         DataGridView1 = New DataGridView()
         Panel2 = New Panel()
-        Button2 = New Button()
-        Button1 = New Button()
-        Label1 = New Label()
-        Label2 = New Label()
-        Label3 = New Label()
-        Label4 = New Label()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
         Label5 = New Label()
         Label6 = New Label()
         Label7 = New Label()
         Label8 = New Label()
-        TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
-        TextBox3 = New TextBox()
-        TextBox4 = New TextBox()
-        TextBox5 = New TextBox()
-        TextBox6 = New TextBox()
-        TextBox7 = New TextBox()
-        TextBox8 = New TextBox()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        Button2 = New Button()
+        Button1 = New Button()
         Panel1.SuspendLayout()
         CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         Panel2.SuspendLayout()
@@ -55,7 +55,7 @@ Partial Class editTenant
         Panel1.Controls.Add(DataGridView1)
         Panel1.Location = New Point(2, 83)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(546, 516)
+        Panel1.Size = New Size(596, 603)
         Panel1.TabIndex = 0
         ' 
         ' DataGridView1
@@ -68,13 +68,13 @@ Partial Class editTenant
         DataGridView1.Name = "DataGridView1"
         DataGridView1.ReadOnly = True
         DataGridView1.RowTemplate.Height = 25
-        DataGridView1.Size = New Size(522, 510)
+        DataGridView1.Size = New Size(591, 597)
         DataGridView1.TabIndex = 0
         ' 
         ' Panel2
         ' 
-        Panel2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
-        Panel2.BackColor = Color.White
+        Panel2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Right
+        Panel2.BackColor = Color.MistyRose
         Panel2.BorderStyle = BorderStyle.Fixed3D
         Panel2.Controls.Add(TextBox7)
         Panel2.Controls.Add(TextBox8)
@@ -94,17 +94,169 @@ Partial Class editTenant
         Panel2.Controls.Add(Label1)
         Panel2.Controls.Add(Button2)
         Panel2.Controls.Add(Button1)
-        Panel2.Location = New Point(553, 15)
+        Panel2.Location = New Point(639, 83)
         Panel2.Name = "Panel2"
-        Panel2.Size = New Size(369, 584)
+        Panel2.Size = New Size(396, 540)
         Panel2.TabIndex = 1
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.BackColor = Color.White
+        TextBox7.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox7.Location = New Point(129, 402)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(229, 28)
+        TextBox7.TabIndex = 17
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.BackColor = Color.White
+        TextBox8.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox8.Location = New Point(129, 360)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(229, 28)
+        TextBox8.TabIndex = 16
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.BackColor = Color.White
+        TextBox5.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox5.Location = New Point(129, 317)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(229, 28)
+        TextBox5.TabIndex = 15
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.BackColor = Color.White
+        TextBox6.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox6.Location = New Point(129, 273)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(229, 28)
+        TextBox6.TabIndex = 14
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BackColor = Color.White
+        TextBox3.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox3.Location = New Point(129, 228)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(229, 28)
+        TextBox3.TabIndex = 13
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BackColor = Color.White
+        TextBox4.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox4.Location = New Point(129, 180)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(229, 28)
+        TextBox4.TabIndex = 12
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.White
+        TextBox2.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox2.Location = New Point(129, 130)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(229, 28)
+        TextBox2.TabIndex = 11
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.White
+        TextBox1.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox1.Location = New Point(129, 79)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(229, 28)
+        TextBox1.TabIndex = 10
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(48, 284)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(50, 17)
+        Label5.TabIndex = 9
+        Label5.Text = "ID No."
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(48, 328)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(42, 17)
+        Label6.TabIndex = 8
+        Label6.Text = "Email"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(10, 371)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(87, 17)
+        Label7.TabIndex = 7
+        Label7.Text = "N.O.K Name"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(7, 413)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(90, 17)
+        Label8.TabIndex = 6
+        Label8.Text = "N.O.K Phone"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(19, 239)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(78, 17)
+        Label4.TabIndex = 5
+        Label4.Text = "Nationality"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(27, 191)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(75, 17)
+        Label3.TabIndex = 4
+        Label3.Text = "Phone No."
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(48, 141)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 17)
+        Label2.TabIndex = 3
+        Label2.Text = "Tel No."
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(5, 90)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(92, 17)
+        Label1.TabIndex = 2
+        Label1.Text = "Tenant Name"
         ' 
         ' Button2
         ' 
         Button2.Anchor = AnchorStyles.Bottom
         Button2.BackColor = Color.RosyBrown
         Button2.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Button2.Location = New Point(255, 533)
+        Button2.Location = New Point(269, 489)
         Button2.Name = "Button2"
         Button2.Size = New Size(103, 32)
         Button2.TabIndex = 1
@@ -116,162 +268,19 @@ Partial Class editTenant
         Button1.Anchor = AnchorStyles.Bottom
         Button1.BackColor = Color.RosyBrown
         Button1.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Button1.Location = New Point(98, 533)
+        Button1.Location = New Point(112, 489)
         Button1.Name = "Button1"
         Button1.Size = New Size(103, 32)
         Button1.TabIndex = 0
         Button1.Text = "Edit"
         Button1.UseVisualStyleBackColor = False
         ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label1.Location = New Point(5, 15)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(92, 17)
-        Label1.TabIndex = 2
-        Label1.Text = "Tenant Name"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label2.Location = New Point(48, 66)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(54, 17)
-        Label2.TabIndex = 3
-        Label2.Text = "Tel No."
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label3.Location = New Point(27, 116)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(75, 17)
-        Label3.TabIndex = 4
-        Label3.Text = "Phone No."
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label4.Location = New Point(48, 164)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(49, 17)
-        Label4.TabIndex = 5
-        Label4.Text = "Label4"
-        ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label5.Location = New Point(48, 209)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(49, 17)
-        Label5.TabIndex = 9
-        Label5.Text = "Label5"
-        ' 
-        ' Label6
-        ' 
-        Label6.AutoSize = True
-        Label6.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label6.Location = New Point(48, 253)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(49, 17)
-        Label6.TabIndex = 8
-        Label6.Text = "Label6"
-        ' 
-        ' Label7
-        ' 
-        Label7.AutoSize = True
-        Label7.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label7.Location = New Point(48, 296)
-        Label7.Name = "Label7"
-        Label7.Size = New Size(49, 17)
-        Label7.TabIndex = 7
-        Label7.Text = "Label7"
-        ' 
-        ' Label8
-        ' 
-        Label8.AutoSize = True
-        Label8.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        Label8.Location = New Point(48, 338)
-        Label8.Name = "Label8"
-        Label8.Size = New Size(49, 17)
-        Label8.TabIndex = 6
-        Label8.Text = "Label8"
-        ' 
-        ' TextBox1
-        ' 
-        TextBox1.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox1.Location = New Point(129, 10)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(229, 28)
-        TextBox1.TabIndex = 10
-        ' 
-        ' TextBox2
-        ' 
-        TextBox2.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox2.Location = New Point(129, 55)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(229, 28)
-        TextBox2.TabIndex = 11
-        ' 
-        ' TextBox3
-        ' 
-        TextBox3.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox3.Location = New Point(129, 153)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(229, 28)
-        TextBox3.TabIndex = 13
-        ' 
-        ' TextBox4
-        ' 
-        TextBox4.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox4.Location = New Point(129, 105)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(229, 28)
-        TextBox4.TabIndex = 12
-        ' 
-        ' TextBox5
-        ' 
-        TextBox5.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox5.Location = New Point(129, 242)
-        TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(229, 28)
-        TextBox5.TabIndex = 15
-        ' 
-        ' TextBox6
-        ' 
-        TextBox6.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox6.Location = New Point(129, 198)
-        TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(229, 28)
-        TextBox6.TabIndex = 14
-        ' 
-        ' TextBox7
-        ' 
-        TextBox7.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox7.Location = New Point(129, 327)
-        TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(229, 28)
-        TextBox7.TabIndex = 17
-        ' 
-        ' TextBox8
-        ' 
-        TextBox8.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox8.Location = New Point(129, 285)
-        TextBox8.Name = "TextBox8"
-        TextBox8.Size = New Size(229, 28)
-        TextBox8.TabIndex = 16
-        ' 
         ' editTenant
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(925, 605)
+        BackColor = Color.WhiteSmoke
+        ClientSize = New Size(1047, 692)
         Controls.Add(Panel2)
         Controls.Add(Panel1)
         FormBorderStyle = FormBorderStyle.None
